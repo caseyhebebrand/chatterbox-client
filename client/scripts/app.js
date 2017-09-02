678a856c0fc1396a22933634485688f803959f27
Hack Reactor Student <student@hackreactor.com>
2017-09-01 20:07:39 -0700

    fetch access to master object

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,73 +1,95 @@
 // YOUR CODE HERE:
 //http://parse.sfm6.hackreactor.com/
-var app = {
+//$(document).ready(function() {
+  var app = {
   
-  server: 'http://parse.sfm6.hackreactor.com/chatterbox/',
-
-  init: function() {},
-  
-  send: function(message) {
-    var context = this;
-    $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages/',
-      type: 'POST',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message sent', data);
-      },
-      error: function (error) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', error);
-      }
-    });
-  },
-
-  fetch: function() {
-    var context = this;
-    $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages/',
-      type: 'GET',
-      
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: fetch sent', data);
-      },
-      error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', data);
-      }
-    });
-
-  },
-
-  clearMessages: function(data) {
-  // might need to remove JUST children note chats 
-    $('#chats').empty();
-  },
-
-  renderMessage: function(message) {
-    var $user = $('<div></div>');
-    $user.text(message.username);
-    var $text = $('<div></div>');
-    $text.text(message.text);
-    var $message = $('<div class=message></div>');
-    $user.appendTo($message);
-    $text.appendTo($message);
-    $message.data('roomName', message.roomname);
+    server: 'http://parse.sfm6.hackreactor.com/chatterbox/',
+    friends: {},
     
-    $message.appendTo('#chats');
-  },
 
-  renderRoom: function(roomName) {
-    var $chatRoom = $('<div></div>');
-    $chatRoom.data('roomName', roomName);
+    init: function() {
+      // fetch();
+    },
 
-    $chatRoom.appendTo('#roomSelect');
-  }
-};
+    send: function(message) {
+      var context = this;
+      $.ajax({
+        // This is the url you should use to communicate with the parse API server.
+        url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages/',
+        type: 'POST',
+        data: JSON.stringify(message),
+        contentType: 'application/json',
+        success: function (data) {
+          console.log('chatterbox: Message sent', data);
+        },
+        error: function (error) {
+          // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+          console.error('chatterbox: Failed to send message', error);
+        }
+      });
+    },
+
+    fetch: function() {
+      
+      var result = $.ajax({
+        // This is the url you should use to communicate with the parse API server.
+        url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages/',
+        type: 'GET',
+        
+        contentType: 'application/json',
+        success: function (data) {
+          console.log('chatterbox: fetch sent', data.results);
+          return data;
+          
+        },
+        error: function (data) {
+          // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+          console.error('chatterbox: Failed to send message', data);
+        }
+      });
+      return result;
+    },
+
+    clearMessages: function(data) {
+    // might need to remove JUST children note chats 
+      $('#chats').empty();
+    },
+
+    renderMessage: function(message) {
+      var $user = $('<div></div>');
+      $user.text(message.username);
+      $user.addClass('username');
+      $user.data('username', message.username);
+      var $text = $('<div></div>');
+      $text.text(message.text);
+      var $message = $('<div class=message></div>');
+      $user.appendTo($message);
+      $text.appendTo($message);
+      $message.data('roomName', message.roomname);
+      
+      $message.appendTo('#chats');
+    },
+
+    renderRoom: function(roomName) {
+      var $chatRoom = $('<div></div>');
+      $chatRoom.data('roomName', roomName);
+
+      $chatRoom.appendTo('#roomSelect');
+    }
+  };
+
+  app.init();
+
+  $('.username').on('click', function() {
+    var user = this.data('username');
+    app.friends[user] = user;
+  });
+  
+//});
+
+
+
+
 
 
 // var message = {
@@ -76,8 +98,7 @@ var app = {
 //         roomname: 'lobby'
 //       };
 
-// app.send(message);
-// app.fetch()
-
+// // app.send(message);
+// console.log(app.fetch());
   
 
